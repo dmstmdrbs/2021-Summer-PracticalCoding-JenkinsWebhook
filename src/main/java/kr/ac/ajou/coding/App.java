40303749fb8705dde61ba23816f21bbc27eb98f2
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-07-11 01:32:09 +0900

    Add slack notification test

--- src/main/java/kr/ac/ajou/coding/App.java
+++ src/main/java/kr/ac/ajou/coding/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("This is Team6's Maven Project");
         System.out.println("Test push");
+        System.out.println("Test Slack Notification");
     }
 }
